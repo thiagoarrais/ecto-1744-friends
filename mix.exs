6291405f68c79d5a442b1b73999c8380f3273a27
Thiago Arrais <thiago.arrais@gmail.com>
2016-10-21 09:52:29 -0300

    The bug reproduces only in 1.1.9

--- mix.exs
+++ mix.exs
@@ -28,7 +28,7 @@ defmodule Friends.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:ecto, "~> 2.0"},
+    [{:ecto, "~> 1.1.8"},
      {:postgrex, "~> 0.11"}]
   end
 end
